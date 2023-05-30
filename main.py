2f6cb2592efa69134d3137a273bfdb5183a7ccde
vh64g <vh64g@gmx.de>
2023-05-30 22:07:54 +0200

    RPS one line filter invalid input

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import random
-while True: print(uc:=input("Your choice:"), print(cc:=f"Computer choice: {random.choice(['Rock', 'Paper', 'Scissor'])}"), print(r:=f"{uc[0]}{cc[17]}".upper(), print("You win") if (r=="RS" or r=="PR" or r=="SP") else print("You Lose") if (r=="RP" or r=="PS" or r=="SR") else print("Draw"), end="\r"), end="\r")
+while True: print(uc:=input("Your choice:"), print(cc:=f"Computer choice: {random.choice(['Rock', 'Paper', 'Scissor'])}"), print(r:=f"{uc[0]}{cc[17]}".upper(), print("You win") if (r=="RS" or r=="PR" or r=="SP") else print("You Lose") if (r=="RP" or r=="PS" or r=="SR") else print("Draw") if (r=="RR" or r=="PP" or r=="SS") else print("Invalid Input"), end="\r"), end="\r")

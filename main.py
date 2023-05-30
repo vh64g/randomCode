d2907d05d2194bf67c039db343fb18161cd6d6e3
vh64g <vh64g@gmx.de>
2023-05-30 22:00:23 +0200

    RPS one line

---
+++ main.py
@@ -0,0 +1,2 @@
+import random
+while True: print(uc:=input("Your choice:"), print(cc:=f"Computer choice: {random.choice(['Rock', 'Paper', 'Scissor'])}"), print(r:=f"{uc[0]}{cc[17]}".upper(), print("You win") if (r=="RS" or r=="PR" or r=="SP") else print("You Lose") if (r=="RP" or r=="PS" or r=="SR") else print("Draw"), end="\r"), end="\r")
